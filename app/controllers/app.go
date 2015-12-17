ffc67100c44e306c9ca9e351322b2b503563f4ce
kkimu <kimura.kzm@gmail.com>
2015-12-17 14:28:56 +0900

    Clean

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -5,24 +5,22 @@ import "github.com/robfig/revel"
 type App struct {
 	*revel.Controller
 }
-type User struct {
-    Name string
-}
 
 func (c App) Index() revel.Result {
-    return c.RenderJson(User{"go tarou"})
+	greeting := "Aloha World"
+	return c.Render(greeting)
 }
 
 
 func (c App) Hello(myName string) revel.Result {
-    c.Validation.Required(myName).Message("Your name is required!")
-    c.Validation.MinSize(myName, 3).Message("Your name is not long enough!")
+  c.Validation.Required(myName).Message("Your name is required!")
+  c.Validation.MinSize(myName, 3).Message("Your name is not long enough!")
 
-    if c.Validation.HasErrors() {
-        c.Validation.Keep()
-        c.FlashParams()
-        return c.Redirect(App.Index)
-    }
+  if c.Validation.HasErrors() {
+    c.Validation.Keep()
+    c.FlashParams()
+    return c.Redirect(App.Index)
+  }
 
-    return c.Render(myName)
+  return c.Render(myName)
 }
